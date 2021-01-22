f94cc8bed613ade0f62cb3911f671029eb121081
Andrei Alvarez <andreialvarezf@gmail.com>
2021-01-22 10:20:46 -0500

    array of object, map and props

--- src/index.js
+++ src/index.js
@@ -4,45 +4,52 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-// setuo vars 
-const title ="Eyes that kiss in the corners";
+// array of books
+
+const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/S/gladiator-image-upload-prod/1/ATVPDKIKX0DER/4d9160f47a2efbf591ca16fbe1a07979._CR0,0,476,583_AC_SL180_QL70_.jpg",
+    title: "Eyes that kiss in the corners",
+    author: "Johanna Ho",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71N4oeWwYlL._AC_UL320_.jpg",
+    title: "The Wonky Donkey",
+    author: "Craig Smith",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/91laaRz2A2L._AC_UL320_.jpg",
+    title: "The World Needs More Purple People",
+    author: "Kristen Bell & Benjamin Hart",
+  },
+];
 
 function BookList() {
-
   return (
     <section className="booklist">
-      <Book title={title}/>
-      <Book job='developer' number={22}> <p>hdckhdkhdskhadckdsaghkhadgskhdsagkhadsgh</p></Book>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} book={book} />;
+      })}
     </section>
   );
 }
 
-const Book = ({title, children}) => {
-  console.log(title);
-  const author = 'Johanna Ho';
+const Book = (props) => {
+  const { title, author, img } = props.book;
   return (
-    <article className='book'>
-      <Image />
-      <Title />
-      <h4 style={{marginTop: '0.25rem', letterSpacing: '5px'}}>{author.toUpperCase()}</h4>
-      <p style={{color: "blue"}}>{title}</p>
-      {children}
+    <article className="book">
+      <img src={img} alt="book" />
+      <h1>{title}</h1>
+      <h4 style={{ marginTop: "0.25rem", letterSpacing: "5px" }}>
+        {author.toUpperCase()}
+      </h4>
+      <p style={{ color: "blue" }}>{title}</p>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/S/gladiator-image-upload-prod/1/ATVPDKIKX0DER/4d9160f47a2efbf591ca16fbe1a07979._CR0,0,476,583_AC_SL180_QL70_.jpg"
-    alt="book"
-  />
-);
-
-const Title = () => <h1>Eyes that kiss in the corners</h1>;
- 
 ReactDom.render(<BookList />, document.getElementById("root"));
